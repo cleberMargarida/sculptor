a91d845fc3c5f0cc51505daab4b83f880e9f3ad0
cleberMargarida <cleber.margarida@outlook.com>
2025-02-08 10:11:15 -0300

    ~chore: Update version and refactor Orleans extensions

--- src/Sculptor.Orleans/OrleansInterceptorExtensions.cs
+++ src/Sculptor.Orleans/OrleansInterceptorExtensions.cs
@@ -1,22 +1,24 @@
 using Microsoft.Extensions.DependencyInjection;
 using Orleans;
+using Orleans.Hosting;
+using Sculptor.Core;
 
 namespace Sculptor.Orleans
 {
     /// <summary>
-    /// Provides extension methods for adding Orleans interceptors to the service collection.
+    /// Extension methods for configuring the SiloBuilder.
     /// </summary>
-    public static class OrleansInterceptorExtensions
+    public static class SiloBuilderExtensions
     {
         /// <summary>
-        /// Adds Orleans interceptors to the specified service collection.
+        /// Adds a filter to the incoming grain call pipeline that sets the ServiceProvider for the current context.
         /// </summary>
-        /// <param name="services">The service collection to add the interceptors to.</param>
-        /// <returns>The service collection with the interceptors added.</returns>
-        public static IServiceCollection AddOrleansInterceptors(this IServiceCollection services)
+        /// <param name="builder">The ISiloBuilder to configure.</param>
+        /// <returns>The configured ISiloBuilder.</returns>
+        public static ISiloBuilder AddRichModelIncomingGrainCallFilter(this ISiloBuilder builder) => builder.AddIncomingGrainCallFilter(static context =>
         {
-            services.AddSingleton<IIncomingGrainCallFilter, SculptorIncomingGrainCallFilter>();
-            return services;
-        }
+            ServiceProviderAccessor.ServiceProvider = context.TargetContext.ActivationServices;
+            return context.Invoke();
+        });
     }
 }
